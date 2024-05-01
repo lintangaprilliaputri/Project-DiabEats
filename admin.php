7b7c7922f5ce190aea3d74b0fec89228e89e368a
BilqisAuraa <162786058+BilqisAuraa@users.noreply.github.com>
2024-05-01 17:37:51 +0700

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food Website</title>
+    <link rel="stylesheet" href="styleAdmin.css">
+</head>
+<body>
+
+    <section id="Home">
+        <nav>
+            <div class="logo">
+                <img src="image/logo DiabEats.png">
+            </div>
+
+            <ul>
+                <li><a href="#Home">Beranda</a></li>
+                <li><a href="#Pesanan">Pesanan</a></li>
+                <li><a href="#Stok">Stok Produk</a></li>
+                <li><a href="#Laporan">Laporan</a></li>
+                <li><a href="logout.php">Logout</a></li>                
+            </ul>
+
+            <div class="icon">
+                <img src="image/Pesan.png">
+                <img src="image/Profil.png">
+            </div>
+        </nav>
+    </section>
+    <?php
+    session_start();
+    // Apabila langsung mengakses admin.php tanpa login, maka akan diarahkan ke index.php untuk login terlebih dahulu
+    if (!isset($_SESSION['user'])) {
+        
+        // Menuju index.php
+        header("location:login.php");
+        exit;
+    }
+    ?>
+</body>
+</html>
---
+++ owner.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food Website</title>
+    <link rel="stylesheet" href="styleOwner.css">
+</head>
+<body>
+
+    <section id="Home">
+        <nav>
+            <div class="logo">
+                <img src="image/logo DiabEats.png">
+            </div>
+
+            <ul>
+                <li><a href="#Home">Beranda</a></li>
+                <li><a href="#Laporan">Laporan</a></li>
+                <li><a href="#Promosi">Promosi & Diskon</a></li>
+                
+            </ul>
+
+            <div class="icon">
+                <img src="image/Profil.png">
+                
+            </div>
+        </nav>
+    
